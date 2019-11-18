47e6bb4119d3c4c250743f0d35461ae0593e12dc
Andrés Losada <nedrocoli@gmail.com>
2019-11-18 13:57:54 -0500

    Listo trabajo en clase

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import JobsList from "./components/jobsList";
+import { IntlProvider } from 'react-intl';
+import localeEs from './locales/es';
+import localeEn from './locales/en';
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+ReactDOM.render(
+    <IntlProvider locale = {navigator.language} messages = {detectarArchivo(navigator.language)}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
+
+function detectarArchivo(lang){
+    console.log(lang)
+    let temp = lang.split("-")
+    if (temp[0] === 'es'){
+        return localeEs;
+    }
+    else if (temp[0] === 'en'){
+        return localeEn;
+    }
+}
